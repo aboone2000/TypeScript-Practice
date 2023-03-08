bd70a95042095d55314367a13b11cfe4150eba66
aboone2000 <aboone2000@gmail.com>
2023-03-08 12:55:12 -0500

    Lesson 3

---
+++ index.ts
@@ -0,0 +1,20 @@
+//TypeScript Basics
+let character = 'mario'
+let age = 30
+let isBlackBelt = false
+
+character = 'cluigi'
+
+//age = 'Yoshi'
+
+age = 40
+
+//isBlackBelt = 'yes'
+isBlackBelt = true
+
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI
+}
+
+console.log(circ(7.5))
