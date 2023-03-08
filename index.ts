67c872f9d0b25e038fd07c91005ca15529a4f52d
aboone2000 <aboone2000@gmail.com>
2023-03-08 12:21:44 -0500

    Lesson 2

---
+++ index.ts
@@ -0,0 +1,13 @@
+//Compiling TypeScript to JavaScript
+
+const character = 'mario'
+
+console.log(character)
+
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs)
+
+inputs.forEach(input =>{
+    console.log(input)
+})
